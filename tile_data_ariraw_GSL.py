2fc418b77815bf8079a753d42ef767767ed20120
George Liu <gsliu11@gmail.com>
2020-01-29 16:03:38 -0800

    Load and tile ariraw unprocessed images (from .mat files from arriread.m)
    Redo CNN training on unprocessed ariraw .ari images, instead of TIFF images, which was done previously in 2019

--- tile_data_ariraw_GSL.py
+++ tile_data_ariraw_GSL.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Aug 14 14:40:06 2019
+Created on Tue Jan 28 17:27:56 2020
 
-Manually tile image into square patches. Tile only portion of image contained within segmentation mask.
+Manually tile ARIRAW image into 32x32 square patches. Tile only portion of image contained within segmentation mask.
 Alternative to dataloading_arriGSL.py
 
 Before running:
     -Update CSV file with latest tissue dataset: arritissue_sessions.csv (C:/Users/CTLab/Documents/George/Python_data/arritissue_data)
 
 @author: CTLab
-Last edit: 8-25-19 - updated tile shape from 36x36 (error) -> (32x32). All previous models trained on 36x36 tiles! 
+Last edit: 1/28/2020 - adapted from tile_data_GSL.py to import .mat data of ARIRAW image RGB demosaiced unprocessed images.
 George Liu
 
 Dependencies: dataloading_arriGSL.py, mat.py
@@ -24,75 +24,22 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import imageio
 import pickle
+import hdf5storage # hdf5storage.loadmat(filepath)  function reads in .mat file into python variable space
 
 PATH_DATA = 'C:/Users/CTLab/Documents/George/Python_data/arritissue_data/'
 PATH_MASK = os.path.join(PATH_DATA, 'masks/')
-# Tile training data
-PATH_IM = os.path.join(PATH_DATA, 'train/')
-#PATH_CSV = os.path.join(PATH_DATA, 'arritissue_sessions.csv')
-PATH_CSV = os.path.join(PATH_DATA, 'arritissue_sessions_test.csv')
-#PATH_PATCHES2D = os.path.join(PATH_DATA, 'train_patches2d/')
-#PATH_PATCHES3D = os.path.join(PATH_DATA, 'train_patches3d/')
-# Tile val data
-#PATH_IM = os.path.join(PATH_DATA, 'val/')
-#PATH_CSV = os.path.join(PATH_DATA, 'arritissue_sessions_val.csv')
-PATH_PATCHES2D = os.path.join(PATH_DATA, 'patches2d/')
-PATH_PATCHES3D = os.path.join(PATH_DATA, 'patches3d/')
-
-myclasses = ["Artery",
-"Bone",
-"Cartilage",
-"Dura",
-"Fascia",
-"Fat",
-"Muscle",
-"Nerve",
-"Parotid",
-"PerichondriumWCartilage",
-"Skin",
-"Vein"]
-num_classes = len(myclasses)
+PATH_IM = os.path.join(PATH_DATA, 'arriraw_data/')
+#PATH_CSV = os.path.join(PATH_DATA, 'arritissue_sessions.csv') # tile training dataset
+#PATH_CSV = os.path.join(PATH_DATA, 'arritissue_sessions_test.csv') # tile test dataset
+PATH_CSV = os.path.join(PATH_DATA, 'arritissue_sessions_val.csv') # tile validation dataset
+PATH_PATCHES2D = os.path.join(PATH_DATA, 'arriraw_data/patches2d/')
+PATH_PATCHES3D = os.path.join(PATH_DATA, 'arriraw_data/patches3d/')
 
 # Fraction of tissue in mask 
 MY_FRACINMASK = 1
 
+
 #%% 8-14-19: Define function to tile image into patches
-
-def gettiles2d(im, mask, tile_size=(32,32), fracinmask=0.5):
-    """Composes several transforms together.
-
-    Args:
-        im (2D numpy array): image
-        mask (2D numpy array): logical array of where tissue is.
-        tile_size (tuple): size of tile
-        fracinmask (double): minimum fraction of mask that must be included in tile
-    
-    Output:
-        cache_tiles (list): tiles of image in mask
-    """ 
-        
-    # Initialize sizes of image and tile    
-    (height, width) = np.shape(im)
-    (tile_height, tile_width) = tile_size
-    
-    # Tile indices
-    (n_y, n_x) = np.floor(np.divide(np.shape(im), tile_size))
-    n_y = int(n_y)
-    n_x = int(n_x)
-    
-    # Iterate through tiles
-    cache_tiles = []
-    for i in range(n_y):
-        yloc = int(i*tile_size[0]) # top-left corner
-        for j in range(n_x):
-            xloc = int(j*tile_size[1])
-            tile = im[yloc:yloc+tile_height, xloc:xloc+tile_width]
-            
-            # Keep tile if in mask (enough)
-            if np.average(mask[yloc:yloc+tile_height, xloc:xloc+tile_width]) >= fracinmask:
-                cache_tiles.append(tile)
-            
-    return cache_tiles
 
 def gettiles3d(im, mask, tile_size=(32,32), fracinmask=0.5):
     """Composes several transforms together.
@@ -131,15 +78,25 @@ def gettiles3d(im, mask, tile_size=(32,32), fracinmask=0.5):
     return cache_tiles
 
 #%% Utilities
+def import_ariraw_matlab(filename):
+    """
+    Import ariraw images, from .mat files by import_ari2mat_GSL.m (Matlab script). Multispectral image is a 21 channel image. 
+    
+    Output: ndarray
+    """ 
+    ariraw_im = hdf5storage.loadmat(filename)
+    ariraw_RGBim = ariraw_im['arriRGB_21channels']
+    return ariraw_RGBim
+
 def save_tilesRGB(list_tiles, target_folder, session_date, this_tissue):
     """
     Save 3-channel, non-multispectral image
     
-    Output: TIFF image
+    Output: pickle file
     """ 
     for i, tile in enumerate(list_tiles):
-        filename = str(session_date) + '_' + str(this_tissue) + '_' + str(i) + '.tiff'
-        imageio.imwrite(os.path.join(target_folder, this_tissue, filename), tile[:,:,:3])
+        filename = str(session_date) + '_' + str(this_tissue) + '_' + str(i) + '.pkl'
+        pickle.dump(tile[:,:,:3], open(os.path.join(target_folder, this_tissue, filename), "wb" ))
         
 def save_tilesMS(list_tiles, target_folder, session_date, this_tissue):
     """
@@ -151,58 +108,9 @@ def save_tilesMS(list_tiles, target_folder, session_date, this_tissue):
         filename = str(session_date) + '_' + str(this_tissue) + '_' + str(i) + '.pkl'
         pickle.dump(tile, open(os.path.join(target_folder, this_tissue, filename), "wb" ))
 
-#%% Test scripts
-def test_gettiles2d():
-    """
-    Test gettiles2d()
-    """ 
-    
-    im1 = np.reshape(np.arange(36), (6,6))
-    mask = np.array([[ 0,  0,  1,  1,  1,  0],
-       [ 0,  0,  8,  9, 0, 0],
-       [12, 13, 14, 15, 16, 17],
-       [18, 19, 20, 21, 22, 23],
-       [0, 0, 26, 27, 0, 0],
-       [0, 0, 32, 33, 0, 0]])
-    mask = mask>0
-    
-    tile_size = (2,2)
-    im1_tiles = gettiles2d(im1, mask, tile_size)
-    print('Original image:')
-    print(im1)
-    print('Tiles of size ' + str(tile_size) +':')
-    print('Num tiles: ' + str(len(im1_tiles)))
-    for i in range(len(im1_tiles)):
-        print(im1_tiles[i])
-
-def test_gettiles3d():
-    print('Image:')
-    filepath1 = mat.uigetfile()
-    this_image = mpimg.imread(filepath1)
-    print('Image dims: ' + str(np.ndim(this_image)))
-    print('Image shape: ' + str(np.shape(this_image)))
-    
-    print('Mask:')
-    filepath2 = mat.uigetfile()
-    this_mask = mpimg.imread(filepath2)
-    print('Mask dims: ' + str(np.ndim(this_mask)))
-    print('Mask shape: ' + str(np.shape(this_mask)))
-    
-    im2_tiles = gettiles3d(this_image, this_mask)
-#    im2_tiles = gettiles3d(this_image, np.ones(np.shape(this_image))) # no mask
-    
-    print('Original image:')
-    plt.figure()
-    plt.imshow(this_image)
-    print('Num tiles: ' + str(len(im2_tiles)))
-    for i in range(len(im2_tiles)):
-        plt.figure()
-        plt.imshow(im2_tiles[i])
 
 #%% Main function
 def main():
-#    test_gettiles2d()
-#    test_gettiles3d()
    
     # PANDAS indexing of tissues by acqusition session, skipping missing tissue samples not acquired in some sessions
     sessions_frame = pd.read_csv(PATH_CSV)
@@ -225,16 +133,14 @@ def main():
         print('Working on image ' + str(idx) + ' out of ' + str(num_images) + ': ' + str(this_session) + ' ' + this_tissue)
         
         # Load image
-        # Concatenate 7 narrow-band 3-channel images (RGBx1) into 1 multispectral 21-channel image (RGBx7)
-        this_folder = os.path.join(root_dir, str(this_session), this_tissue)
-        this_tiffs = [f for f in os.listdir(this_folder) if f.endswith('.tif')]
+        this_folder = os.path.join(root_dir, str(this_session))
+        this_mats = [f for f in os.listdir(this_folder) if f.endswith('.mat')]
         image = np.nan
-        for img_name in this_tiffs:
-            this_image = mpimg.imread(os.path.join(this_folder, img_name)) # read image
-            if np.isnan(np.sum(image)): # no concatenation necessary for 1st image
-                image = this_image
-            else:
-                image = np.concatenate((image, this_image), axis = 2)
+        for k in range(len(this_mats)):
+            mat_filename = this_mats[k]
+            if this_tissue in mat_filename:
+                image = import_ariraw_matlab(os.path.join(this_folder, mat_filename))
+                break
                 
         # load mask
         segmentation_folder = os.path.join(mask_dir, str(this_session))
@@ -257,3 +163,5 @@ def main():
 
 if __name__=='__main__':
     main()
+#    filepath = mat.uigetfile()
+#    ariraw_im = import_ariraw_matlab(filepath) 

bb7be1062a514919aafaf87ab02c27f7a69fdb09
George Liu <gsliu11@gmail.com>
2020-04-30 18:48:19 -0700

    Update evaluate_arrinet_wholeimage.py

--- evaluate_arrinet_wholeimage.py
+++ evaluate_arrinet_wholeimage.py
@@ -27,6 +27,11 @@ import matplotlib as mpl
 import seaborn as sns
 import pickle
 from copy import deepcopy
+import sklearn
+import sklearn.metrics
+import numpy.matlib
+from scipy import interp
+from itertools import cycle
 
 NAME_PERICHONDRIUM = 'PerichondriumWCartilage'
 NAME_CARTILAGE = 'Cartilage' 
@@ -39,6 +44,18 @@ PATH_EVAL_OUTPUT = 'C:/Users/CTLab/Documents/George/Python_data/arritissue_data/
 NUM_CLASSES = 11
 
 TISSUE_CLASSES = ["Background", "Artery",
+"Bone",
+"Cartilage",
+"Dura",
+"Fascia",
+"Fat",
+"Muscle",
+"Nerve",
+"Parotid",
+"Skin",
+"Vein"]
+
+TISSUE_CLASSES_FINAL = ["Artery",
 "Bone",
 "Cartilage",
 "Dura",
@@ -188,8 +205,14 @@ def evaluate_bigimage(path_image):
     segmentation_filename = [f for f in os.listdir(segmentation_folder) if f.lower().endswith((this_tissue + 'Mask.png').lower())]
     assert len(segmentation_filename)>0, "Number of segmentation files is: " + str(len(segmentation_filename)) + " in folder " + str(segmentation_folder) + " for tissue " + str(this_tissue)
     segmentation = mpimg.imread(os.path.join(segmentation_folder, str(segmentation_filename[0])))
+    
+    # # 4-30-20: compare performance of ARRInet-W with ARRInet-M using just 3 channels and other channels occluded
+    # im_occluded = deepcopy(im) # copy; avoid assignment by reference
+    # im_occluded[:,:,3:] = MEAN_CHANNEL_PIXELVALS[3:] # occlude entire i'th channel with average value across training dataset
+    
     # Tile image into 32x32 pixel patches
     list_tiles, list_fracmask_intile, list_loc = tile_data_ariraw_GSL.gettiles3d(im, segmentation, tile_size=(32,32), fracinmask=0)
+    # list_tiles, list_fracmask_intile, list_loc = tile_data_ariraw_GSL.gettiles3d(im_occluded, segmentation, tile_size=(32,32), fracinmask=0)
     stack_tiles = np.stack(list_tiles, axis=0) # 4-D ndarray of shape (N=1980, H=32, W=32, C=21)
     stack_tiles = np.transpose(stack_tiles, axes=(0, 3, 1, 2)) # permute dimensions from ndarray (N, H, W, C) to (N, C, H, W) 
 
@@ -310,24 +333,24 @@ def evaluate_bigimage(path_image):
     pickle_filename_RGB = 'arrinet_RGB_analysis.pickle'
     pickle.dump([class_scores_RGB, class_prob_RGB, pred_class_int_RGB, pred_class_name_RGB, list_fracmask_intile, list_loc, segmentation], open(os.path.join(target_folder, pickle_filename_RGB), "wb" ))
 
-    # # Save figures
-    # # Figure saving parameters
-    # FIG_HEIGHT = 12 # inches
-    # FIG_WIDTH = 8 # inches
-    # FIG_DPI = 200
+    # Save figures
+    # Figure saving parameters
+    FIG_HEIGHT = 12 # inches
+    FIG_WIDTH = 8 # inches
+    FIG_DPI = 200
     
-    # filename_id = date + '_' + this_tissue + '_labelis_' + true_class + '_' + dataset
-    # fig0_filename = 'tiff_' + filename_id + '.pdf'
-    # fig1_filename = 'heatmap_ms_pred' + filename_id + '_acc_' + str(np.around(frac_tilesinmask_correct, decimals=0)) + '.pdf'
-    # fig2_filename = 'heatmap_ms_probability' + filename_id + '_acc_' + str(np.around(prob_trueclass_inmask_ms*100, decimals=0)) + '.pdf'
-    # fig3_filename = 'heatmap_rgb_pred' + filename_id + '_acc_' + str(np.around(frac_tilesinmask_correct_RGB, decimals=0)) + '.pdf'
-    # fig4_filename = 'heatmap_rgb_probability' + filename_id + '_acc_' + str(np.around(prob_trueclass_inmask_RGB*100, decimals=0)) + '.pdf'
+    filename_id = date + '_' + this_tissue + '_labelis_' + true_class + '_' + dataset
+    fig0_filename = 'tiff_' + filename_id + '.pdf'
+    fig1_filename = 'heatmap_ms_pred' + filename_id + '_acc_' + str(np.around(frac_tilesinmask_correct, decimals=0)) + '.pdf'
+    fig2_filename = 'heatmap_ms_probability' + filename_id + '_acc_' + str(np.around(prob_trueclass_inmask_ms*100, decimals=0)) + '.pdf'
+    fig3_filename = 'heatmap_rgb_pred' + filename_id + '_acc_' + str(np.around(frac_tilesinmask_correct_RGB, decimals=0)) + '.pdf'
+    fig4_filename = 'heatmap_rgb_probability' + filename_id + '_acc_' + str(np.around(prob_trueclass_inmask_RGB*100, decimals=0)) + '.pdf'
     
-    # fig0.set_size_inches(FIG_HEIGHT, FIG_WIDTH)
-    # fig1.set_size_inches(FIG_HEIGHT, FIG_WIDTH)
-    # fig2.set_size_inches(FIG_HEIGHT, FIG_WIDTH)
-    # fig3.set_size_inches(FIG_HEIGHT, FIG_WIDTH)
-    # fig4.set_size_inches(FIG_HEIGHT, FIG_WIDTH)
+    fig0.set_size_inches(FIG_HEIGHT, FIG_WIDTH)
+    fig1.set_size_inches(FIG_HEIGHT, FIG_WIDTH)
+    fig2.set_size_inches(FIG_HEIGHT, FIG_WIDTH)
+    fig3.set_size_inches(FIG_HEIGHT, FIG_WIDTH)
+    fig4.set_size_inches(FIG_HEIGHT, FIG_WIDTH)
     
     # fig0.savefig(os.path.join(target_folder, fig0_filename), bbox_inches='tight', dpi=FIG_DPI)
     # fig1.savefig(os.path.join(target_folder, fig1_filename), bbox_inches='tight', dpi=FIG_DPI)
@@ -594,7 +617,7 @@ def compare_parotid_nerve(path_folder):
     # cache_frac_tilesinmask_correct_occluded_nerve = deepcopy(cache_frac_tilesinmask_correct_occluded)
     # cache_prob_trueclass_inmask_occluded_nerve = deepcopy(cache_prob_trueclass_inmask_occluded)
 
-    # TODO: Analyze occlusion of parotid vs nerve
+    # Analyze occlusion of parotid vs nerve
     diff_parotid_occlusions_pred = parotid_occlusions_pred - parotid_frac_tilesinmask_correct
     diff_parotid_occlusions_prob = parotid_occlusions_prob - ave_prob_parotid
     diff_nerve_occlusions_pred = nerve_occlusions_pred - nerve_frac_tilesinmask_correct
@@ -647,6 +670,8 @@ def plot_histogram_rawimage(path_image):
     
     Args:
         path_image (str) - path to .mat file containing raw tissue multispectral image
+        
+    TODO: this code function is incomplete as of 4-30-2020
     """
     # Read in image to python workspace
     im = tile_data_ariraw_GSL.import_ariraw_matlab(path_image) # 21 channel multispectral image, float64 ndarray shape (H, W, C) 
@@ -669,6 +694,149 @@ def plot_histogram_rawimage(path_image):
 
     plt.show()        
     print('done')
+
+def roc_arrinet(path_folder):
+    """Generate ROC curves and evaluate AUC of Arrinet classification of directory of whole raw images (eg test dataset).
+    Compare performance of multispectral and non-multispectral data.
+    
+    Args:
+        path_folder (string) - path to a directory containing .mat files of raw big image data.
+        
+    Output:
+        none
+    """
+    
+    # Initialize true and predicted label vectors
+    n_classes = len(TISSUE_CLASSES_FINAL)
+    y_true = np.array([], dtype=np.int64).reshape(0,n_classes) 
+    y_score_ms = np.array([], dtype=np.int64).reshape(0,n_classes) 
+    y_score_rgb = np.array([], dtype=np.int64).reshape(0,n_classes) 
+    
+    # Read in raw images in folder path
+    only_matfiles = [f for f in os.listdir(path_folder) if f.endswith(".mat")]
+    num_files = len(only_matfiles)
+    for i, file in enumerate(only_matfiles):
+        print('\n Working on image', str(i), 'out of', str(num_files))
+        path_image = os.path.join(path_folder, file)
+    
+        # Read in image to python workspace
+        im = tile_data_ariraw_GSL.import_ariraw_matlab(path_image) # 21 channel multispectral image, float64 ndarray shape (H, W, C) 
+        
+        # Read in acquisition date
+        filename = os.path.basename(path_image)
+        date = filename[0:8]
+        
+        # Read in true tissue class
+        pattern = "-(.*?)_"
+        this_tissue = re.search(pattern, filename).group(1)
+        # relabel true class as cartilage if tissue type is perichondrium
+        if this_tissue.lower() == NAME_PERICHONDRIUM.lower():
+            true_class = NAME_CARTILAGE
+        else:
+            true_class = this_tissue
+              
+        # Read in which dataset the image belongs to (train, validation, or test)
+        datasplit_key = pd.read_csv(PATH_DATASPLIT_KEY)
+        boolcol_session = datasplit_key["session"]==int(date)
+        myrow = [i for i, x in enumerate(boolcol_session) if x]
+        dataset = datasplit_key["Split"][myrow[0]]
+        
+        # Tile image, including foreground and background
+        # load mask
+        segmentation_folder = os.path.join(PATH_MASK, str(date))
+        segmentation_filename = [f for f in os.listdir(segmentation_folder) if f.lower().endswith((this_tissue + 'Mask.png').lower())]
+        assert len(segmentation_filename)>0, "Number of segmentation files is: " + str(len(segmentation_filename)) + " in folder " + str(segmentation_folder) + " for tissue " + str(this_tissue)
+        segmentation = mpimg.imread(os.path.join(segmentation_folder, str(segmentation_filename[0])))
+        
+        # Tile image into 32x32 pixel patches
+        list_tiles, list_fracmask_intile, list_loc = tile_data_ariraw_GSL.gettiles3d(im, segmentation, tile_size=(32,32), fracinmask=0)
+        stack_tiles = np.stack(list_tiles, axis=0) # 4-D ndarray of shape (N=1980, H=32, W=32, C=21)
+        stack_tiles = np.transpose(stack_tiles, axes=(0, 3, 1, 2)) # permute dimensions from ndarray (N, H, W, C) to (N, C, H, W) 
+    
+        # Obtain multispectral classification scores and predictions for all tiles in whole image
+        class_scores, class_prob, pred_class_int, pred_class_name = arrinet_classify.classify(stack_tiles, isprocessed=False, ismultispectral=True)
+        
+        # Obtain non-multispectral classification scores and predictions
+        class_scores_RGB, class_prob_RGB, pred_class_int_RGB, pred_class_name_RGB = arrinet_classify.classify(stack_tiles[:,:3,:,:], isprocessed=False, ismultispectral=False)
+
+        # Create vector of binarized true class label
+        num_tiles = class_scores.shape[0]
+        true_class = np.matlib.repmat(true_class, num_tiles, 1)
+        this_label = sklearn.preprocessing.label_binarize(true_class, classes=TISSUE_CLASSES_FINAL)
+        print(np.shape(this_label))
+        print(np.shape(y_true))
+
+        # Update true and predicted label vectors
+        y_true = np.concatenate((y_true, this_label), axis=0) 
+        y_score_ms = np.concatenate((y_score_ms, class_scores), axis=0) 
+        y_score_rgb = np.concatenate((y_score_rgb, class_scores_RGB), axis=0) 
+        
+    print('Analyzing ROC....')
+    # Compute ROC curve and ROC area for each class
+    y_test = y_true
+    for y_score in [y_score_ms, y_score_rgb]:
+        fpr = dict()
+        tpr = dict()
+        roc_auc = dict()
+        for i in range(n_classes):
+            fpr[i], tpr[i], _ = sklearn.metrics.roc_curve(y_test[:, i], y_score[:, i])
+            roc_auc[i] = sklearn.metrics.auc(fpr[i], tpr[i])
+        
+        # Compute micro-average ROC curve and ROC area
+        fpr["micro"], tpr["micro"], _ = sklearn.metrics.roc_curve(y_test.ravel(), y_score.ravel())
+        roc_auc["micro"] = sklearn.metrics.auc(fpr["micro"], tpr["micro"])
+        
+        # First aggregate all false positive rates
+        all_fpr = np.unique(np.concatenate([fpr[i] for i in range(n_classes)]))
+        
+        # Then interpolate all ROC curves at this points
+        mean_tpr = np.zeros_like(all_fpr)
+        for i in range(n_classes):
+            mean_tpr += interp(all_fpr, fpr[i], tpr[i])
+        
+        # Finally average it and compute AUC
+        mean_tpr /= n_classes
+        
+        fpr["macro"] = all_fpr
+        tpr["macro"] = mean_tpr
+        roc_auc["macro"] = sklearn.metrics.auc(fpr["macro"], tpr["macro"])
+        
+        # Plot all ROC curves
+        fig0 = plt.figure()
+        plt.plot(fpr["micro"], tpr["micro"],
+                 label='micro-average ROC curve (area = {0:0.2f})'
+                       ''.format(roc_auc["micro"]),
+                 color='deeppink', linestyle=':', linewidth=4)
+        
+        plt.plot(fpr["macro"], tpr["macro"],
+                 label='macro-average ROC curve (area = {0:0.2f})'
+                       ''.format(roc_auc["macro"]),
+                 color='navy', linestyle=':', linewidth=4)
+        
+        colors = cycle(['aqua', 'darkorange', 'cornflowerblue'])
+        lw = 2
+        for i, color in zip(range(n_classes), colors):
+            plt.plot(fpr[i], tpr[i], color=color, lw=lw,
+                     label='ROC curve of class {0} (area = {1:0.2f})'
+                     ''.format(i, roc_auc[i]))
+        
+        plt.plot([0, 1], [0, 1], 'k--', lw=lw)
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title('Some extension of Receiver operating characteristic to multi-class')
+        plt.legend(loc="lower right")
+            # Save figures
+    
+        plt.show()
+        
+        # Figure saving parameters
+        FIG_HEIGHT = 12 # inches
+        FIG_WIDTH = 8 # inches
+        fig0.set_size_inches(FIG_HEIGHT, FIG_WIDTH)
+    
+    # TODO: Calculate significant difference statistic of AUC ARRInet-M vs ARRInet-W
 
 def main():
     # #User select a whole raw image (.mat)
@@ -703,11 +871,17 @@ def main():
     # print(path_outputdir)
     # compare_parotid_nerve(path_outputdir)
     
-    # 4-17-20: Plot histogram of channels for user-selected raw multispectral image
-    print('Select an unprocessed, multispectral whole image (.mat):')
-    path_image = mat.uigetfile(initialdir='C:/Users/CTLab/Documents/George/Python_data/arritissue_data/', title='Select file', filetypes=(("MAT-file","*.mat"),("all files","*.*")))
-    print(path_image)
-    plot_histogram_rawimage(path_image)
+    # # 4-17-20: Plot histogram of channels for user-selected raw multispectral image
+    # print('Select an unprocessed, multispectral whole image (.mat):')
+    # path_image = mat.uigetfile(initialdir='C:/Users/CTLab/Documents/George/Python_data/arritissue_data/', title='Select file', filetypes=(("MAT-file","*.mat"),("all files","*.*")))
+    # print(path_image)
+    # plot_histogram_rawimage(path_image)
+    
+    # 4-30-20: Calculate ROC and AUC of ARRInet classifiers to compare performance
+    print('Select a folder containing unprocessed, multispectral whole images (.mat files). Folder name should be acquisition date:')
+    path_dir = mat.uigetdir(initialdir='C:/Users/CTLab/Documents/George/Python_data/arritissue_data/', title='Select folder')
+    print(path_dir)
+    roc_arrinet(path_dir)
     
     print('Done.')
     
